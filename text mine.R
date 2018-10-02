3b2fe2c7ac7dd5fd60edf5cee789267e3fee7010
binayak91 <um18280@stu.ximb.ac.in>
2018-10-02 16:20:27 +0530

    2nd oct

---
+++ text mine.R
@@ -0,0 +1,30 @@
+# library
+library(wordcloud2) 
+
+# have a look to the example dataset
+head(demoFreq)
+
+wordcloud2(demoFreq, size=1.6)
+
+# Gives a proposed palette
+wordcloud2(demoFreq, size=1.6, color='random-dark')
+
+# or a vector of colors. vector must be same length than input data
+wordcloud2(demoFreq, size=1.6, color=rep_len( c("green","blue"), nrow(demoFreq) ) )
+
+# Change the background color
+wordcloud2(demoFreq, size=1.6, color='random-light', backgroundColor="black")
+
+
+# Install
+install.packages("tm")  # for text mining
+install.packages("SnowballC") # for text stemming
+install.packages("wordcloud") # word-cloud generator 
+install.packages("RColorBrewer") # color palettes
+# Load
+library("tm")
+library("SnowballC")
+library("wordcloud")
+library("RColorBrewer")
+
+text <- readLines(file.choose())
